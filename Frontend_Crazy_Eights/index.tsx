613841b95b867a55b79704f574c3a6a9477b0e6c
Thanh Do <thanhdo@wless-user-10190243215.vancouver.wsu.edu>
2024-04-17 09:56:36 -0700

    Add React and HTML to index.tsx

--- Frontend_Crazy_Eights/index.tsx
+++ Frontend_Crazy_Eights/index.tsx
@@ -1,4 +1,6 @@
 import readline from 'readline-sync';
+import React, { StrictMode, Component, ReactNode } from "react";
+import { createRoot } from "react-dom/client";
 
 enum Rank {
 	Ace = 0x1,
@@ -331,6 +333,70 @@ class Game {
 	}
 
 }
+
+interface MyListProps {
+    maxItems?: number;
+    children?: React.JSX.Element | React.JSX.Element[];
+}
+
+class MyListState {
+    nClicks : number = 0;
+}
+
+class MyList extends Component<MyListProps, MyListState> {
+    constructor(props: MyListProps) {
+        super(props);
+        this.state = new MyListState();
+        this.addClick = this.addClick.bind(this);
+    }
+
+    override render(): ReactNode {
+        const children = React.Children.toArray(this.props.children);
+        const result = []
+        const nChildren = this.props.maxItems ?? children.length;
+    
+        for( let child = 0; child < Math.min(nChildren, children.length); child++ ) {
+            result.push( children[child] );
+        }
+
+        result.push( <li>You have clicked {this.state.nClicks} times.</li> );
+
+        return <ul onClick={this.addClick}>{result}</ul>
+    }
+
+    addClick(): void {
+        let newState = new MyListState();
+        newState.nClicks = this.state.nClicks + 1;
+        this.setState( newState );
+    }
+}
+
+class App extends Component {
+    override render(): ReactNode {
+        return <div>
+            <p>Crazy Eights</p>
+            <MyList maxItems={2}>
+                <li>one</li>
+                <li>two</li>
+                <li>three</li>
+            </MyList>
+        </div>
+    }
+}
+
+const rootElem = document.getElementById('root');
+
+if( rootElem == null ) {
+    alert('you forgot to put a root element in your HTML file.');
+}
+
+const root = createRoot( rootElem as HTMLElement );
+
+root.render(
+    <StrictMode>
+        <App/>
+    </StrictMode>
+);
 
 let g = new Game();
 g.hostGame();
--- Frontend_Crazy_Eights/src/index.jsx
+++ Frontend_Crazy_Eights/src/index.jsx
@@ -1,9 +1,34 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const readline_sync_1 = __importDefault(require("readline-sync"));
+const react_1 = __importStar(require("react"));
+const client_1 = require("react-dom/client");
 var Rank;
 (function (Rank) {
     Rank[Rank["Ace"] = 1] = "Ace";
@@ -288,6 +313,54 @@ class Game {
         }
     }
 }
+class MyListState {
+    constructor() {
+        this.nClicks = 0;
+    }
+}
+class MyList extends react_1.Component {
+    constructor(props) {
+        super(props);
+        this.state = new MyListState();
+        this.addClick = this.addClick.bind(this);
+    }
+    render() {
+        var _a;
+        const children = react_1.default.Children.toArray(this.props.children);
+        const result = [];
+        const nChildren = (_a = this.props.maxItems) !== null && _a !== void 0 ? _a : children.length;
+        for (let child = 0; child < Math.min(nChildren, children.length); child++) {
+            result.push(children[child]);
+        }
+        result.push(<li>You have clicked {this.state.nClicks} times.</li>);
+        return <ul onClick={this.addClick}>{result}</ul>;
+    }
+    addClick() {
+        let newState = new MyListState();
+        newState.nClicks = this.state.nClicks + 1;
+        this.setState(newState);
+    }
+}
+class App extends react_1.Component {
+    render() {
+        return <div>
+            <p>Crazy Eights</p>
+            <MyList maxItems={2}>
+                <li>one</li>
+                <li>two</li>
+                <li>three</li>
+            </MyList>
+        </div>;
+    }
+}
+const rootElem = document.getElementById('root');
+if (rootElem == null) {
+    alert('you forgot to put a root element in your HTML file.');
+}
+const root = (0, client_1.createRoot)(rootElem);
+root.render(<react_1.StrictMode>
+        <App />
+    </react_1.StrictMode>);
 let g = new Game();
 g.hostGame();
 g.printGameStatus();
